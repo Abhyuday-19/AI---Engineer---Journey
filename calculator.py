7cfa7234b9063e65fd8f6580a9d453531243b5aa
Abhyuday kumar <mail.abhyudaykumar@gmail.com>
2025-12-14 17:20:23 +0530

    small calculator

---
+++ calculator.py
@@ -0,0 +1,8 @@
+Number1=int(input("Enter the first number:"))
+Number2=int(input("Enter the second number:"))
+
+print("The sum of the numbers is:",Number1+Number2)
+print("The difference of the numbers is:",Number1-Number2)
+print("The product of the numbers is:",Number1*Number2)
+print("The division of the numbers is:",Number1/Number2)
+

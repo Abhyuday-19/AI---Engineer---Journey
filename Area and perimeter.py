430561b2800447f8b1d2966d535b029ccdf23bd4
Abhyuday kumar <mail.abhyudaykumar@gmail.com>
2025-12-14 17:14:47 +0530

    Area and perimeter of reactangle

---
+++ Area and perimeter.py
@@ -0,0 +1,6 @@
+Length=int(input("Enter the length of the rectangle:"))
+Breadth=int(input("Enter the breadth of the rectangle:"))
+Area=Length*Breadth
+perimeter=2*(Length+Breadth)
+print("The area of the rectangle is:",Area,"cm^2")
+print("The perimeter of the rectangle is:",perimeter,"cm")

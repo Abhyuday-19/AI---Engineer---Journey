b73e863e08c946e12d135bc629d58c776d62d46b
Abhyuday kumar <mail.abhyudaykumar@gmail.com>
2025-12-16 20:29:45 +0530

    person is eligible to vote

---
+++ eligible.py
@@ -0,0 +1,6 @@
+person=input("Enter the name of person:")
+Age=int(input("Enter the age of person:"))
+if(Age>=18):
+    print("Person is eligible to vote")
+else:
+    print("Person is not eligible to vote")

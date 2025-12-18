5ad805dbc41bc5909463b8d92dcb79cb43caf145
Abhyuday kumar <mail.abhyudaykumar@gmail.com>
2025-12-18 19:40:32 +0530

    Print numbers from 10 to 1 using while loop

--- loop.py
+++ loop.py
@@ -2,4 +2,4 @@ i=5
 while i>=1:
     print(i)
     i=i-1
-print("Loop ended")
+print("Loop ended","And you are doing great abhyuday this consistency is fare better than motivation")
---
+++ question on loop.py
@@ -0,0 +1,5 @@
+i=10
+while i>=1:
+    print(i)
+    i=i-1
+print("counting in reverse order of counting 10 to 1")

253d57296048cf61c57010890d238cf375e6b43c
Abhyuday kumar <mail.abhyudaykumar@gmail.com>
2025-12-31 17:38:08 +0530

    addition of numbers by using function.

---
+++ function.py
@@ -0,0 +1,6 @@
+def add_numbers():
+    number1=int(input("Enter the first number:"))
+    number2=int(input("Enter the second number:"))
+    addition=number1+number2
+    print("The addition of the numbers is :",addition)
+add_numbers()

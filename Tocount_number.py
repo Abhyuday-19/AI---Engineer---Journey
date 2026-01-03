66f630147daf6c8d2e549474589ecbbee86e1c3b
Abhyuday kumar <mail.abhyudaykumar@gmail.com>
2026-01-03 14:38:17 +0530

    To print triangle pattern in right hand side.

--- Tocount_number.py
+++ Tocount_number.py
@@ -1,6 +1,6 @@
 def count_digit():
     for i in range(1,6):
-        for j in range(1,i+1):
-            print("*",end="  ")
+        for j in range("*",i+1):
+            print(j,end=" ")
         print()
 count_digit()
---
+++ pattern_printing2.py
@@ -0,0 +1,14 @@
+def pattern_printing():
+    i=0
+    while i<=5:
+        b=1
+        while b<=5-i:
+            print("  ",end="")
+            b=b+1
+        j=1
+        while j<=i:
+            print("*",end=" ")
+            j=j+1
+        print()
+        i=i+1
+pattern_printing()
